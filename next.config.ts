17020bd6c451ef2b78b11e1d27b311f84569d34d
Maina Wanjiru <wanjirumaina539@gmail.com>
2025-05-22 09:29:57 +0000

    i dont see any photos

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,18 @@ const nextConfig: NextConfig = {
         hostname: 'source.unsplash.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
