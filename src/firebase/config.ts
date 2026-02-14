8812c7b56bcb17db93011cb26029dcb1532bb7a3
Maina Wanjiru <wanjirumaina539@gmail.com>
2026-02-14 10:48:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "simon-styles-limited",
+  "appId": "1:105541216160:web:3c8ba629e8745afca5e6c5",
+  "apiKey": "AIzaSyAOd3j292aQ-YHLUo-ztX0ysF4iXuustDE",
+  "authDomain": "simon-styles-limited.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "105541216160"
+};
